5c317c6e178b531f588d7daeea67d6e1930a027c
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2024-11-26 16:28:25 +0800

    feat: setup tailwind color config, setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
 function App() {
   return (
-    <>
-      <div>
-        <h1 className="text-2xl text-red-400">Hello</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        primary: "#5F33E1",
+        primaryDark: "#2A098A",
+        secondary: "#EDE8FF",
+        textPrimary: "#24252C",
+        textSecondary: "#6E6A7C",
+        lightPink: "F478B8",
+        darkPink: "#F478B8",
+        bgMain: "#FBFCFC",
+        cardBg: "#FFFFFF",
+        darkBlue: "#0087FF",
+        lightBlue: "#E7F3FF",
+        darkOrange: "#FF7D53",
+        lightOrange: "#FFE9E1",
+      },
+      container: {
+        center: true,
+        padding: {
+          default: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
   },
   plugins: [],
 };
