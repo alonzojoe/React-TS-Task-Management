ecc92fea2d322f754bacf804c97dfcf60cfdc9fe
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2024-12-11 15:31:56 +0800

    feat: implement search filter, by name and title, create custom hook useDebounce

--- src/pages/Tasks/TaskList.tsx
+++ src/pages/Tasks/TaskList.tsx
@@ -3,12 +3,15 @@ import useTask from "../../store/tasks-context";
 import { type TaskData } from "../../types/Task";
 import { STATUS, CATEGORIES } from "../../constants/global";
 import TaskFilter from "./components/TaskFilter";
+import useDebounce from "../../hooks/useDebounce";
 
 import Lists from "./components/Lists";
 
 const TaskList = () => {
   const { tasks } = useTask();
   const [filter, setFilter] = useState<string | null>(null);
+  const [searchFilter, setSearchFilter] = useState("");
+  const debounceValue = useDebounce(searchFilter, 500);
 
   const mappedTasks: TaskData[] | null = useMemo(() => {
     if (!tasks || tasks.length === 0) return null;
@@ -20,15 +23,34 @@ const TaskList = () => {
         status: STATUS.find((s) => s.id === task.status) || null,
       }))
       .filter((task) => {
-        if (!filter) return true;
-        return task?.status?.name === filter;
+        if (filter && task?.status?.name !== filter) return false;
+
+        if (
+          debounceValue &&
+          !(
+            task?.title?.toLowerCase().includes(debounceValue.toLowerCase()) ||
+            task?.description
+              ?.toLowerCase()
+              .includes(debounceValue.toLowerCase())
+          )
+        ) {
+          return false;
+        }
+
+        return true;
       });
-  }, [tasks, filter]);
+  }, [tasks, filter, debounceValue]);
 
   // console.log("mapped task", mappedTasks);
+  console.log(searchFilter);
   return (
     <div className="container space-y-5  py-2">
-      <TaskFilter filter={filter} onFilter={setFilter} />
+      <TaskFilter
+        searchFilter={searchFilter}
+        onSearch={setSearchFilter}
+        filter={filter}
+        onFilter={setFilter}
+      />
       <Lists tasks={mappedTasks} />
     </div>
   );
--- src/pages/Tasks/components/TaskFilter.tsx
+++ src/pages/Tasks/components/TaskFilter.tsx
@@ -1,14 +1,33 @@
 import React from "react";
 import { FILTER_STATUS } from "../../../constants/global";
+import { IoSearch } from "react-icons/io5";
 
 type TaskFilterProps = {
   filter: string | null;
   onFilter: React.Dispatch<React.SetStateAction<string | null>>;
+  searchFilter: string;
+  onSearch: React.Dispatch<React.SetStateAction<string>>;
 };
 
-const TaskFilter = ({ filter, onFilter }: TaskFilterProps) => {
+const TaskFilter = ({
+  filter,
+  onFilter,
+  searchFilter,
+  onSearch,
+}: TaskFilterProps) => {
   return (
     <div className="overflow-x-auto whitespace-nowrap py-4">
+      <div className="mb-4 relative group">
+        <input
+          type="text"
+          value={searchFilter}
+          onChange={(e) => onSearch(e.target.value)}
+          className="w-full py-1 text-lg px-2 text-textPrimary outline-none border-lightPrimary focus:border-primary rounded-xl border-[1px]"
+        />
+        <span className="bg-transparent absolute right-2 top-2">
+          <IoSearch className="text-xl text-lightPrimary group-focus-within:text-primary" />
+        </span>
+      </div>
       <div className="flex space-x-4">
         {FILTER_STATUS.map((stat) => (
           <button
